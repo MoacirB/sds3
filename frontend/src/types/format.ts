e3b7dd3309b5f6d28236183efaa4329038ffece2
MoacirB <moacir-batista-tavares@hotmail.com>
2021-05-11 18:57:13 -0300

    Pagination

--- frontend/src/types/format.ts
+++
@@ -1,5 +0,0 @@
-//Arredonda números para 1 casa decimal
-export const round = (value: number, precision: number) => {
-    var multiplier = Math.pow(10, precision || 0);
-    return Math.round(value * multiplier) / multiplier;
-}
